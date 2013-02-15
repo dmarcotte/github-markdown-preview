3adf92096c80f80502439f2581fa358c166fed30
David Simon <dsimon@americancouncils.org>
2013-02-15 12:40:52 -0500

    Error message if no arguments supplied

--- github-markdown-preview.rb
+++ github-markdown-preview.rb
@@ -4,6 +4,11 @@ require 'rubygems'
 require 'listen'
 require 'html/pipeline'
 
+unless ARGV.count == 1
+  puts "Please supply the name of a markdown file as the first agument."
+  exit 1
+end
+
 watched_file = File.expand_path(ARGV.at(0))
 watched_file_dir = File.dirname(watched_file)
 preview_file = watched_file + '.html'
