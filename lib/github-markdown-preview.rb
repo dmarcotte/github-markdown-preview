c510aa552810d60df7323286914db18878393e4c
Daniel Marcotte <dmarcotte@gmail.com>
2013-12-31 11:01:19 -0800

    Render task lists in comment mode
    Enhance the comment preview to simulate the task lists that Github
    creates for "- [ ] task" and "- [x] completed task" list items

--- lib/github-markdown-preview.rb
+++ lib/github-markdown-preview.rb
@@ -1,8 +1,8 @@
-
 module GithubMarkdownPreview
   class FileNotFoundError < StandardError; end
 
   require 'github-markdown-preview/version'
   require 'github-markdown-preview/resources'
   require 'github-markdown-preview/html_preview'
+  require 'github-markdown-preview/filter/task_list_filter'
 end
